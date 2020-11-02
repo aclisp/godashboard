bde0f38cff89c26f34b24e54ab4563f251edd79e
huanghao <huanghao@yy.com>
2020-11-02 21:48:15 +0800

    add login form

--- frontend/view/login.go
+++ frontend/view/login.go
@@ -32,14 +32,124 @@ func (c *Login) Render() vecty.ComponentOrHTML {
 						vecty.Markup(
 							vecty.Class("column", "is-5-tablet", "is-4-desktop", "is-3-widescreen"),
 						),
-						elem.Form(
-							vecty.Markup(
-								vecty.Class("box"),
-							),
-							vecty.Text("Login"),
+						c.renderForm(),
+					),
+				),
+			),
+		),
+	)
+}
+
+func (c *Login) renderForm() *vecty.HTML {
+	return elem.Form(
+		vecty.Markup(
+			vecty.Class("box"),
+		),
+		elem.Div(
+			vecty.Markup(
+				vecty.Class("field", "has-text-centered"),
+			),
+			elem.Image(
+				vecty.Markup(
+					vecty.Property("src", "/images/logo-bis.png"),
+					vecty.Style("width", "167"),
+				),
+			),
+		),
+		elem.Div(
+			vecty.Markup(
+				vecty.Class("field"),
+			),
+			elem.Label(
+				vecty.Markup(
+					vecty.Class("label"),
+				),
+				vecty.Text("Email"),
+			),
+			elem.Div(
+				vecty.Markup(
+					vecty.Class("control", "has-icons-left"),
+				),
+				elem.Input(
+					vecty.Markup(
+						vecty.Class("input"),
+						vecty.Property("type", "email"),
+						vecty.Property("placeholder", "e.g. alexjohnson@gmail.com"),
+						vecty.Attribute("required", ""),
+					),
+				),
+				elem.Span(
+					vecty.Markup(
+						vecty.Class("icon", "is-small", "is-left"),
+					),
+					elem.Italic(
+						vecty.Markup(
+							vecty.Class("fa", "fa-envelope"),
 						),
 					),
 				),
+			),
+		),
+		elem.Div(
+			vecty.Markup(
+				vecty.Class("field"),
+			),
+			elem.Label(
+				vecty.Markup(
+					vecty.Class("label"),
+				),
+				vecty.Text("Password"),
+			),
+			elem.Div(
+				vecty.Markup(
+					vecty.Class("control", "has-icons-left"),
+				),
+				elem.Input(
+					vecty.Markup(
+						vecty.Class("input"),
+						vecty.Property("type", "password"),
+						vecty.Property("placeholder", "********"),
+						vecty.Attribute("required", ""),
+					),
+				),
+				elem.Span(
+					vecty.Markup(
+						vecty.Class("icon", "is-small", "is-left"),
+					),
+					elem.Italic(
+						vecty.Markup(
+							vecty.Class("fa", "fa-lock"),
+						),
+					),
+				),
+			),
+		),
+		elem.Div(
+			vecty.Markup(
+				vecty.Class("field"),
+			),
+			elem.Label(
+				vecty.Markup(
+					vecty.Class("checkbox"),
+				),
+				elem.Input(
+					vecty.Markup(
+						vecty.Property("type", "checkbox"),
+						vecty.Attribute("required", ""),
+					),
+				),
+				vecty.Text(" Remember me"),
+			),
+		),
+		elem.Div(
+			vecty.Markup(
+				vecty.Class("field"),
+			),
+			elem.Button(
+				vecty.Markup(
+					vecty.Class("button", "is-success"),
+				),
+				vecty.Text("Login"),
 			),
 		),
 	)
